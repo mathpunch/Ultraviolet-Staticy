3847743df6f55b2194a2d2f9209f095f9c03fede
mathpunch Official <coutsozzy09@gmail.com>
2026-02-12 09:16:36 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,19 +3,20 @@ const input = document.querySelector('input');
 
 form.addEventListener('submit', async event => {
     event.preventDefault();
-    window.navigator.serviceWorker.register('./sw.js', {
+    
+    // Updated path to register the worker from the uv folder
+    window.navigator.serviceWorker.register('./uv/uv.sw.js', {
         scope: __uv$config.prefix
     }).then(() => {
         let url = input.value.trim();
         if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
         else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
 
-
         window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
     });
 });
 
 function isUrl(val = ''){
-    if (/^http(s?):\/\//.test(val) || val.includes('.') && val.substr(0, 1) !== ' ') return true;
+    if (/^http(s?):\/\//.test(val) || (val.includes('.') && val.substr(0, 1) !== ' ')) return true;
     return false;
 };
